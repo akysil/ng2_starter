19baa22789f8925fa734d66ee38556354190ebdb
akysil <alex.kysil@gmail.com>
2017-01-25 11:42:50 -0800

    Import all external styles in style.ts

--- src/main.aot.ts
+++ src/main.aot.ts
@@ -1,8 +1,7 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
-
-import './style.css';
+import './style';
 
 enableProdMode();
 
